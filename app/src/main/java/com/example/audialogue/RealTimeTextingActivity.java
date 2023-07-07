6dd826ecbb17c76d8d1f48b7fe183eecf1ffcf8f
Chen Han <chance.chen.official@gmail.com>
2023-07-07 21:13:19 +0800

    Removed back and toggle buttons, added auto-fullscreen, added center text with auto-scaling

--- app/src/main/java/com/example/audialogue/RealTimeTextingActivity.java
+++ app/src/main/java/com/example/audialogue/RealTimeTextingActivity.java
@@ -3,24 +3,29 @@ package com.example.audialogue;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 public class RealTimeTextingActivity extends AppCompatActivity {
-
-    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_real_time_texting);
-        backButton = findViewById(R.id.backBut);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-            }
-        });
+
+        // Enable full-screen mode
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        );
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        
     }
+
+
 }
