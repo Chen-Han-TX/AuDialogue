fee93c853fa3ed9ed08af383bc9512ba175f3a05
Chen Han <chance.chen.official@gmail.com>
2023-07-07 21:23:24 +0800

    added function to toggle the flip

--- app/src/main/java/com/example/audialogue/RealTimeTextingActivity.java
+++ app/src/main/java/com/example/audialogue/RealTimeTextingActivity.java
@@ -1,16 +1,17 @@
 package com.example.audialogue;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.TextView;
 
 public class RealTimeTextingActivity extends AppCompatActivity {
+
+    private TextView centerText;
+    private boolean isFlipped = false;
+    /*
+    The climax of this course is its final project. The final project is your opportunity to take your newfound savvy with programming out for a spin and develop your very own piece of software. So long as your project draws upon this course’s lessons, the nature of your project is entirely up to you.
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +25,25 @@ public class RealTimeTextingActivity extends AppCompatActivity {
                         View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        
+
+        // Toggle Center Text to flip vertically
+        centerText = findViewById(R.id.center_text);
+        centerText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleFlip();
+            }
+        });
     }
 
-
+    private void toggleFlip() {
+        if (isFlipped) {
+            // Restore to normal
+            centerText.setScaleY(1);
+        } else {
+            // Flip vertically
+            centerText.setScaleY(-1);
+        }
+        isFlipped = !isFlipped;
+    }
 }
