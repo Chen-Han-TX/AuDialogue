168575b4e8a747217fca7c43153b8edffc176d73
Chen Han <chance.chen.official@gmail.com>
2023-07-09 09:21:59 +0800

    Basic functionality done
    Now screen can rotate 180 degrees once the user send a message on the center.
    Moving onto added Back button and History

--- app/src/main/java/com/example/audialogue/RealTimeTextingActivity.java
+++ app/src/main/java/com/example/audialogue/RealTimeTextingActivity.java
@@ -1,14 +1,25 @@
 package com.example.audialogue;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 import android.widget.TextView;
+
 
 public class RealTimeTextingActivity extends AppCompatActivity {
 
     private TextView centerText;
     private boolean isFlipped = false;
+    private EditText editText;
+
     /*
     The climax of this course is its final project. The final project is your opportunity to take your newfound savvy with programming out for a spin and develop your very own piece of software. So long as your project draws upon this course’s lessons, the nature of your project is entirely up to you.
      */
@@ -21,7 +32,6 @@ public class RealTimeTextingActivity extends AppCompatActivity {
         // Enable full-screen mode
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_FULLSCREEN |
-                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
                         View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
         );
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
@@ -34,16 +44,67 @@ public class RealTimeTextingActivity extends AppCompatActivity {
                 toggleFlip();
             }
         });
+
+        // editText at the bottom
+        editText = findViewById(R.id.editText);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // This space intentionally left blank
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Update centerText as the text in editText changes
+                centerText.setText(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // This space intentionally left blank
+            }
+        });
+
+        // Handle the "Send" button on the keyboard
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+
+                    // Save the text in history
+                    String text = String.valueOf(centerText.getText());
+
+
+                    editText.setText("");
+                    centerText.setText(text);
+
+
+                    // Hide keyboard
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    if (imm != null) {
+                        imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+                    }
+                    toggleFlip();
+
+                    handled = true;
+                }
+                return handled;
+            }
+        });
     }
 
     private void toggleFlip() {
         if (isFlipped) {
             // Restore to normal
-            centerText.setScaleY(1);
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         } else {
-            // Flip vertically
-            centerText.setScaleY(-1);
+            // Rotate 180 degrees
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
         }
         isFlipped = !isFlipped;
     }
+    
 }
+
